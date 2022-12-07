3929d4f4e7830f026612095e38d25fff7316af1b
Kamil Kuc <kamil.kuc95@gmail.com>
2022-12-07 19:52:08 +0100

    Install eslint

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
+/*Pytania
+1. z watchem: pokazuja mi errory do poprawienia takie jak za duża ilosc tab
+*/
 'use strict';
 
 function titleClickHandler(event){
-    event.preventDefault();
-    const clickedElement = this;
-    
-    console.log('Link was clicked!');
-    console.log(event);
+  event.preventDefault();
+  const clickedElement = this;
+
+  console.log('Link was clicked!');
+  console.log(event);
 
   /* [DONE] remove class 'active' from all article links  */
-    const activeLinks = document.querySelectorAll('.titles a.active');
-        for(let activeLink of activeLinks){
-            activeLink.classList.remove('active');
-        }
+  const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
   /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
   /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
-        for(let activeArticle of activeArticles){
-            activeArticle.classList.remove('active');
-        }
+      for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+      }
   /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     console.log("Link was clicked! ", articleSelector);
